005603ae81b58fd94b4d1957aab47d73f5d4bf2e
Stephane Philippart <wildagsx@gmail.com>
2022-06-20 23:24:04 +0200

    👋  Hello World

--- controllers/nginxoperator_controller.go
+++ controllers/nginxoperator_controller.go
@@ -19,9 +19,11 @@ package controllers
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	frwildav1 "github.com/philippart-s/breizh-camp-go-operator/api/v1"
@@ -37,19 +39,48 @@ type NginxOperatorReconciler struct {
 //+kubebuilder:rbac:groups=fr.wilda,resources=nginxoperators/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=fr.wilda,resources=nginxoperators/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the NginxOperator object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *NginxOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
+	const helloWorldFinalizer = "fr.wilda/finalizer"
 
-	// TODO(user): your logic here
+	helloWorld := &frwildav1.NginxOperator{}
+	err := r.Get(ctx, req.NamespacedName, helloWorld)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// CR deleted, nothing to do
+			log.Info("No CR found, nothing to do 🧐.")
+		} else {
+			// Error reading the object - requeue the request.
+			log.Error(err, "Failed to get CR NginxOperator")
+			return ctrl.Result{}, err
+		}
+	} else {
+		// Add finalizer for this CR
+		if !controllerutil.ContainsFinalizer(helloWorld, helloWorldFinalizer) {
+			controllerutil.AddFinalizer(helloWorld, helloWorldFinalizer)
+			err = r.Update(ctx, helloWorld)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{}, nil
+		}
+
+		if helloWorld.GetDeletionTimestamp() != nil {
+			// CR marked for deletion ➡️ Goodbye
+			log.Info("Goodbye " + helloWorld.Spec.Name + " 😢")
+			controllerutil.RemoveFinalizer(helloWorld, helloWorldFinalizer)
+			err := r.Update(ctx, helloWorld)
+			if err != nil {
+				log.Info("Error during deletion")
+				return ctrl.Result{}, err
+			}
+		} else {
+			// CR created / updated ➡️ Hello
+			log.Info("Hello " + helloWorld.Spec.Name + " 🎉🎉 !!")
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
